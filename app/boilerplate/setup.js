2684e0b1ac18ec2ab3a822dee28e4ac46094d945
Jarl Silvén <jarl.silven@gmail.com>
2020-05-28 11:43:36 +0200

    :package: Public folder skeleton, for frontend

--- app/boilerplate/setup.js
+++ app/boilerplate/setup.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 module.exports = () => {
 	const app = express();
 	
+	// Declare public directory, starting with index.html
+	app.use(
+		express.static(path.join(__dirname, '..', '..', 'public'))
+		// express.static(absolutePathToPublicDirectory)
+	);
+
 	return {
 		app
 	}
--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ const setupBoilerplate = require('./boilerplate/setup.js');
 const { app } = setupBoilerplate();
 const port = 8989;
 
+// use /api for REST router, to not interfere with regular urls
 const router = require('./controllers/rest.controller.js');
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(port);
