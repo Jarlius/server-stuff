33b5c228f334afa0c28dedf67dadf9502646c33c
Jarl Silvén <jarl.silven@gmail.com>
2020-05-31 20:07:12 +0200

    :hammer: Game board layout with sidebar and blue/red background

--- public/views/blue.view.js
+++ public/views/blue.view.js
@@ -8,9 +8,23 @@ Vue.component('blue', {
 		console.log('blue');
 	},
 	template: `
-	<div class="blueboard">
-		{{ text }}
-		<button v-on:click="$root.redirect('/red')">switch</button>
-	</div>
-	`
+	<div class="grid-container origin">
+		<div class="grid-item" id="sidebar">
+			Ships:<br><br>
+			2: <span id="s1" class="data">0</span><br><br>
+			3: <span id="s2" class="data">0</span><br><br>
+			4: <span id="s3" class="data">0</span><br><br>
+			5: <span id="s4" class="data">0</span><br><br><br>
+			Turn: <span id="turn" class="data">0</span>
+		</div>
+		<div class="grid-item" id="blue-background">
+			<h2>Battleship!</h2>
+			<div class="grid-container gameboard blue">
+				<div class="grid-item">{{ text }}</div>
+				<div class="grid-item">
+					<button v-on:click="$root.redirect('/red')">switch</button>
+				</div>
+			</div>
+		</div>
+	</div>`
 });
--- public/views/red.view.js
+++ public/views/red.view.js
@@ -8,9 +8,23 @@ Vue.component('red', {
 		console.log('red');
 	},
 	template: `
-	<div class = "redboard">
-		{{ text }}
-		<button v-on:click="$root.redirect('/blue')">switch</button>
-	</div>
-	`
+	<div class="grid-container origin">
+		<div class="grid-item" id="sidebar">
+			Ships:<br><br>
+			2: <span id="s1" class="data">0</span><br><br>
+			3: <span id="s2" class="data">0</span><br><br>
+			4: <span id="s3" class="data">0</span><br><br>
+			5: <span id="s4" class="data">0</span><br><br><br>
+			Turn: <span id="turn" class="data">0</span>
+		</div>
+		<div class="grid-item" id="red-background">
+			<h2>Battleship!</h2>
+			<div class="grid-container gameboard red">
+				<div class="grid-item">{{ text }}</div>
+				<div class="grid-item">
+					<button v-on:click="$root.redirect('/blue')">switch</button>
+				</div>
+			</div>
+		</div>
+	</div>`
 });
