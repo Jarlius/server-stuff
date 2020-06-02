60f63b0443daefa136b7107073982647382aefa6
Jarl Silvén <jarl.silven@gmail.com>
2020-06-03 00:33:25 +0200

    :book: Future interface documentation for battleship get function

--- app/controllers/rest.controller.js
+++ app/controllers/rest.controller.js
@@ -2,6 +2,10 @@ const model = require('../model.js');
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Set up all information when refreshing browser
+ * Format: { state, turn, size, blueboard[tile{n,color}], redboard[tile{n,color}] }
+ */
 router.get('/state/:color', function(req, res) {
 	res.json(model.getState(req.params.color));
 });
