9175186106bc3562c66172e3b22adae27b66964a
Jarl Silvén <jarl.silven@gmail.com>
2020-06-02 10:49:40 +0200

    :gear: better-logging for server output stream

--- app/boilerplate/setup.js
+++ app/boilerplate/setup.js
@@ -1,11 +1,19 @@
-const express = require('express');
+require('better-logging')(console);
+
 const path = require('path');
+const express = require('express');
 
 module.exports = () => {
 	const app = express();
-	
-	// Declare public directory, starting with index.html
+
+	// Setup express
+	app.use((req, res, next) => {
+		// Logs each incoming request
+		console.info(`${req.ip} ${req.path} ${req.body || ''}`);
+		next();
+	});
 	app.use(
+		// Declare public directory, starting with index.html
 		express.static(path.join(__dirname, '..', '..', 'public'))
 		// express.static(absolutePathToPublicDirectory)
 	);
