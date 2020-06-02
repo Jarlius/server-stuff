f723fcc3a25322427804fb119a6509155e41320c
Jarl Silvén <jarl.silven@gmail.com>
2020-06-03 00:47:00 +0200

    :hammer: More states for backend

--- app/model.js
+++ app/model.js
@@ -1,5 +1,8 @@
 var state = 'start';
 
+var blue_board = [];
+var red_board = [];
+
 /**
  * Get the state of the battleship model, and whether the player should show the board
  */
@@ -15,7 +18,20 @@ exports.getState = color => {
 exports.nextState = color => {
 	switch (state) {
 	case 'start':
-		state = 'blueprep';
+//		state = 'blueprep';
+		state = 'blueturn';
+		break;
+	case 'blueturn':
+		state = 'blueend';
+		break;
+	case 'blueend':
+		state = 'redturn';
+		break;
+	case 'redturn':
+		state = 'redend';
+		break;
+	case 'redend':
+		state = 'blueturn';
 		break;
 	default:
 		break;
