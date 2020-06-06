fccde7264994be05a25700fee884fcbc9f274342
Jarl Silvén <jarl.silven@gmail.com>
2020-06-06 19:03:14 +0200

    :books: Correct documentation for GET function

--- app/controllers/rest.controller.js
+++ app/controllers/rest.controller.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 /**
  * Set up all information when refreshing browser
- * Format: { state, turn, size, blueboard[tile{n,color}], redboard[tile{n,color}] }
+ * Format: { state, turn, size, [color]score, boards(tile{n,color})[blue, red]
  */
 router.get('/state/:color', function(req, res) {
 	res.json({
 		state: model.getState(),
+		// TODO turns
 		size: model.getSize(),
 		score: model.getScore(req.params.color),
 		boards: model.getBoards(req.params.color)
