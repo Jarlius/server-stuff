f01f729095c612a1f27a3d91e061dedbb9174831
Jarl Silvén <jarl.silven@gmail.com>
2020-06-06 17:11:01 +0200

    :books: Documentation for added code

--- app/index.js
+++ app/index.js
@@ -3,15 +3,17 @@ const setupBoilerplate = require('./boilerplate/setup.js');
 const { app, io, listen } = setupBoilerplate();
 const port = 8989;
 
-// use /api for REST router, to not interfere with regular urls
+// use /api for REST router, to not interfere with regular urls (may be changed)
 const router = require('./controllers/rest.controller.js');
 app.use('/api', router);
 
+// Pair up socket controller with the socket io url
 const socketController = require('./controllers/socket.controller.js');
 io.on('connection', socket => {
 	socketController(socket, io);
 });
 
+// listening using this function lets both the app and io use the same port
 listen(port, () => {
-  console.log("server listening on port", port);
+	console.log("server listening on port", port);
 });
